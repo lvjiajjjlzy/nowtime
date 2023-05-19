a163105be00d3494ec3356dc0e2c75f5c042c51f
lvjiajjjlzy <l1196444919@163.com>
2023-05-19 13:46:52 +0800

    clear b   u ggggggg
    clear b   u ggggggg

---
+++ src/main/java/org/lv/nowtime/Nowdate.java
@@ -0,0 +1,36 @@
+package org.lv.nowtime;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.scoreboard.ScoreboardPlayerScore;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents.START_SERVER_TICK;
+
+
+public class Nowdate {
+    public void tick() {
+        START_SERVER_TICK.register(server -> {
+
+
+            LocalDate currentDate = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+            int data = Integer.parseInt(currentDate.format(formatter));
+
+            List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
+            ScoreboardObjective objective = server.getScoreboard().getObjective("nowdate");
+
+
+            for (PlayerEntity player : players) {
+                String playerName = player.getEntityName();
+                ScoreboardPlayerScore scoreboardPlayerScore = server.getScoreboard().getPlayerScore(playerName, objective);
+                scoreboardPlayerScore.setScore(data);
+
+            }
+        });
+    }
+}
--- src/main/java/org/lv/nowtime/Nowtime.java
+++ src/main/java/org/lv/nowtime/Nowtime.java
@@ -2,15 +2,13 @@ package org.lv.nowtime;
 
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.player.PlayerEntity;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.ScoreboardObjective;
 import net.minecraft.scoreboard.ScoreboardPlayerScore;
 import net.minecraft.server.network.ServerPlayerEntity;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents.START_SERVER_TICK;
@@ -19,11 +17,10 @@ import static net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents.START_
 public class Nowtime extends Scoreboard implements ModInitializer {
 
 
-    private int repeat = 0;
-
     @Override
     public void onInitialize() {
-
+        Nowdate nowdate = new Nowdate();
+        ServerTickEvents.START_SERVER_TICK.register(server -> nowdate.tick());
 
         START_SERVER_TICK.register(server -> {
             LocalTime now = LocalTime.now();
@@ -32,7 +29,7 @@ public class Nowtime extends Scoreboard implements ModInitializer {
             List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
             ScoreboardObjective objective = server.getScoreboard().getObjective("nowtime");
 
-            for (PlayerEntity player : players) {
+            for (ServerPlayerEntity player : players) {
                 String playerName = player.getEntityName();
                 ScoreboardPlayerScore scoreboardPlayerScore = server.getScoreboard().getPlayerScore(playerName, objective);
                 scoreboardPlayerScore.setScore(time);
@@ -40,27 +37,8 @@ public class Nowtime extends Scoreboard implements ModInitializer {
         });
 
 
-        START_SERVER_TICK.register(server -> {
-            repeat++;
-
-            if (repeat == 20) {
-                repeat = 0;
-
-                LocalDate currentDate = LocalDate.now();
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-                int data = Integer.parseInt(currentDate.format(formatter));
-
-                List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
-                ScoreboardObjective objective = server.getScoreboard().getObjective("nowdate");
 
 
-                for (PlayerEntity player : players) {
-                    String playerName = player.getEntityName();
-                    ScoreboardPlayerScore scoreboardPlayerScore = server.getScoreboard().getPlayerScore(playerName, objective);
-                    scoreboardPlayerScore.setScore(data);
-                }
-            }
-        });
  /*       ServerWorldEvents.LOAD.register((server, world) -> {
 commands?
         });
